b069b67e8455660c3c5a8af44713b054532f6a6f
carrel <dvquentincarrel@gmail.com>
2023-03-22 00:11:18 +0100

    Init, separated into its own repo

---
+++ README.md
@@ -0,0 +1,27 @@
+# Q_bashline
+This is a statusline for bash.
+
+## Features
+- Last exit code
+- Current working directory
+- NNN recursion depth
+- Current git branch
+- Working tree status
+
+Due to the time it may take to retrieve informations on large repos, this operation
+is done asynchronously.  
+Due to limitations to either bash or my own knowledge,
+status line gets updated only after every command.
+
+Should the asynchronicity bother you, you could disable it by setting the env var
+$GIT_ASYNC to false.
+
+## Installation
+To wire your bash to use this, run `setup.sh` or add the corresponding lines
+at the end of your bashrc:
+~~~ bash
+coproc CO_NC_GIT { nc -klU /tmp/.QBL_$$; } && disown $!
+git_lineutils $$ & disown $!
+LINEUTILS_PID=$!
+trap "rm -f /tmp/.QBL_$$; kill $LINEUTILS_PID" EXIT
+~~~
---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Setup script, handles symlink creation, handles bashrc updating
+
+update_bashrc=true
+
+# Checks whether it looks like it's already been done. Allow user to abort update
+if $(grep -qF "coproc CO_NC_GIT " "$HOME/.bashrc"); then
+    echo "This configuration seems already present in your bashrc."
+    echo -n "Are you sure you want to continue? y/any: "
+    read answer
+    [[ "$answer" != "y" ]] && update_bashrc=false
+fi
+
+if [[ "$update_bashrc" == "true" ]]; then
+# Adds relevant lines to bashrc
+    cat >> "$HOME/.bashrc" <<EOF
+coproc CO_NC_GIT { nc -klU /tmp/.QBL_\$\$; } && disown \$!
+git_lineutils \$\$ & disown \$!
+LINEUTILS_PID=\$!
+trap "rm -f /tmp/.QBL_\$$; kill \$LINEUTILS_PID" EXIT
+EOF
+    echo "Updated bashrc"
+else
+    echo "Bashrc left as-is"
+fi
+
+lbin="$HOME/.local/bin"
+mkdir -p "$HOME/.local/bin"
+for executable in \
+        bashline_colors drain_socket \
+        git_lineutilsv2 q_bashline; do
+    ln -si "$PWD/$executable" "$lbin/$executable"
+done
