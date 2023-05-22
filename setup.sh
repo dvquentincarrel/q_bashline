a462e6726f626fd3af778e8668f3079b89b46c8a
Quentin Carrel <dvquentincarrel@gmail.com>
2023-05-22 21:41:25 +0000

    [MA] Init file, symlinks handling
    [A] All initialization calls done in a new init file. Only one line in the bashrc now.
    [M] More explicit handling of symlinks (creation / overwrite)

--- setup.sh
+++ setup.sh
@@ -4,28 +4,17 @@
 update_bashrc=true
 
 # Checks whether it looks like it's already been done. Allow user to abort update
-if $(grep -qF "coproc CO_NC_GIT " "$HOME/.bashrc"); then
+if $(grep -qF "source qbl_init" "$HOME/.bashrc"); then
     echo "This configuration seems already present in your bashrc."
-    echo -n "Are you sure you want to continue? y/any: "
+    echo -n "Are you sure you want to modify it? y/any: "
     read answer
     [[ "$answer" != "y" ]] && update_bashrc=false
 fi
 
 if [[ "$update_bashrc" == "true" ]]; then
-# Adds relevant lines to bashrc, ~/.local/bin to path if necessary
-    grep -qv "$HOME/.local/bin" <<< "$PATH" && path_ext='
-export PATH="$PATH:$HOME/.local/bin"'
-    cat >> "$HOME/.bashrc" <<EOF
-
-# ========== Q_BASHLINE
-coproc CO_NC_GIT { trap -- "true" EXIT; nc -klU /tmp/.QBL_\$\$; } && disown \$! $path_ext
-git_lineutils \$\$ & disown \$!
-LINEUTILS_PID=\$!
-trap "rm -f /tmp/.QBL_\$$; kill \$LINEUTILS_PID" EXIT
-source bashline_colors
-PROMPT_COMMAND="source q_bashline"
-# ==========
-EOF
+    # Adds relevant lines to bashrc, ~/.local/bin to path if necessary
+    grep -qv "$HOME/.local/bin" <<< "$PATH" && printf 'export PATH="$PATH:$HOME/.local/bin"' >> "$HOME/.bashrc"
+    printf '\nsource qbl_init' >> "$HOME/.bashrc"
     echo "Updated bashrc"
 else
     echo "Bashrc left as-is"
@@ -33,17 +22,23 @@ fi
 
 lbin="$HOME/.local/bin"
 mkdir -p "$HOME/.local/bin"
-for executable in \
+for executable in qbl_init \
         bashline_colors drain_socket \
         git_lineutils q_bashline; do
-    ln -si "$PWD/$executable" "$lbin/$executable"
+    if [[ -e "$lbin/$executable" ]]; then
+        echo -n "$lbin/$executable already exists. Replace it? y/any: "
+        read answer
+        [[ "$answer" == "y" ]] && ln -sf "$PWD/$executable" "$lbin/$executable" && echo "File overwritten"
+    else
+        ln -s "$PWD/$executable" "$lbin/$executable"
+        echo "Linked $executable"
+    fi
 done
 
 # Asks to replace fancy glyphs if supporting fonts not found
 if ! grep -qPi "nerd|powerglyph" <<< "$(fc-list)"; then
-	r
-	echo 'Font that supports fancy glyphs not found.'
-	echo 'Do you want to use ▐ & ▌ instead of &  ? y/any: '
-	read answer
-	[[ "$answer" == "y" ]] && sed -i -e 's//▐/' -e 's//▌/' q_bashline
+    echo 'Font that supports fancy glyphs not found.'
+    echo -n 'Do you want to use ▐ & ▌ instead of &  ? y/any: '
+    read answer
+    [[ "$answer" == "y" ]] && sed -i -e 's//▐/' -e 's//▌/' q_bashline
 fi
