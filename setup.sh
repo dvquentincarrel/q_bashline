ecc34a8b76918279fbb2332f222334a7ecca6d33
Quentin Carrel <dvquentincarrel@gmail.com>
2023-05-22 20:23:29 +0000

    [FA] Sources colors file too, updates PATH if needed, asks to change glyphs if related fonts missing

--- setup.sh
+++ setup.sh
@@ -12,12 +12,19 @@ if $(grep -qF "coproc CO_NC_GIT " "$HOME/.bashrc"); then
 fi
 
 if [[ "$update_bashrc" == "true" ]]; then
-# Adds relevant lines to bashrc
+# Adds relevant lines to bashrc, ~/.local/bin to path if necessary
+    grep -qv "$HOME/.local/bin" <<< "$PATH" && path_ext='
+export PATH="$PATH:$HOME/.local/bin"'
     cat >> "$HOME/.bashrc" <<EOF
-coproc CO_NC_GIT { trap -- "true" EXIT; nc -klU /tmp/.QBL_\$\$; } && disown \$!
+
+# ========== Q_BASHLINE
+coproc CO_NC_GIT { trap -- "true" EXIT; nc -klU /tmp/.QBL_\$\$; } && disown \$! $path_ext
 git_lineutils \$\$ & disown \$!
 LINEUTILS_PID=\$!
 trap "rm -f /tmp/.QBL_\$$; kill \$LINEUTILS_PID" EXIT
+source bashline_colors
+PROMPT_COMMAND="source q_bashline"
+# ==========
 EOF
     echo "Updated bashrc"
 else
@@ -31,3 +38,12 @@ for executable in \
         git_lineutils q_bashline; do
     ln -si "$PWD/$executable" "$lbin/$executable"
 done
+
+# Asks to replace fancy glyphs if supporting fonts not found
+if ! grep -qPi "nerd|powerglyph" <<< "$(fc-list)"; then
+	r
+	echo 'Font that supports fancy glyphs not found.'
+	echo 'Do you want to use ▐ & ▌ instead of &  ? y/any: '
+	read answer
+	[[ "$answer" == "y" ]] && sed -i -e 's//▐/' -e 's//▌/' q_bashline
+fi
